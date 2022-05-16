45532af2ef3495f2e4638c3f0912d606984161ee
Andrey13042003 <92572052+Andrey13042003@users.noreply.github.com>
2022-05-16 22:04:08 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QFileDialog>
+
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,8 +21,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    int Parsing_File(string path_to_file, string region_name);
+
+private slots:
+    void on_btn_open_file_clicked();
 
 private:
     Ui::MainWindow *ui;
+    Function array[];
 };
 #endif // MAINWINDOW_H
